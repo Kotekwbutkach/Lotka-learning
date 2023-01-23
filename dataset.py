e02dbdc9711ae07b90da8fa759c81b5fd80c2ac4
Paweros <88387004+Paweros@users.noreply.github.com>
2023-01-23 01:22:28 +0100

    Restructured project

--- dataset.py
+++ dataset.py
@@ -1,26 +1,3 @@
-import numpy as np
-import pandas as pd
-from torch.utils.data import Dataset
-
-
-class CustomDataset(Dataset):
-    def __init__(self, datafile, observed_num, predicted_num, transform=None, target_transform=None):
-        self.data = pd.read_csv(datafile, sep=";", header=None)
-        self.observed_num = observed_num  # N
-        self.predicted_num = predicted_num  # k
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, id):
-        observed_values = np.array(self.data.loc[id, :2*self.observed_num-1])
-        predicted_values = np.array(self.data.loc[id, 2*self.observed_num:(2*(self.observed_num+self.predicted_num)-1)])
-
-        if self.transform:
-            observed_values = self.transform(observed_values)
-        if self.target_transform:
-            predicted_values = self.target_transform(predicted_values)
-
-        return observed_values, predicted_values
+version https://git-lfs.github.com/spec/v1
+oid sha256:a146b368cce7df568186caf21f92023eee7f04c6509723b18f9b672b75dd017d
+size 1221
